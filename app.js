bd39d1c8782ab6980fcb61ba0768ffae062a7457
jarvisgit config --global user.name jarvis <13g7895123@gmail.com>
2023-04-07 09:23:30 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// route setting
+// app.get('/', (req, res) => {
+//   res.send('This is my first Express app')
+// })
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+// create server
+app.listen(port, () => {
+  console.log(`server listen to http://localhost:${port}`)
+})
+
+// setting template engine
+app.engine('handlebars', exphbs.engine('defaultLayout: main'))
+app.set('view engine', 'handlebars')
+
+// route setting
+
